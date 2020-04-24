c0676c9f85af56464f6fe5dab2bd961cbf12bd09
Juanmagc99 <juagarcri@alum.us.es>
2020-04-24 20:59:00 +0200

    subiendo repo

---
+++ Carta.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Carta</title>
+</head>
+<body>
+    <?php
+        include_once("Header.html");
+    ?>
+</body>
+</html>
