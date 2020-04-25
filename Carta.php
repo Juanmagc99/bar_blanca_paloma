2e14d60c7a3fc850d119e3235962113359fe875e
Juanmagc99 <juagarcri@alum.us.es>
2020-04-25 20:05:13 +0200

    modificada la vistaincipal de carta pendiente de ver como implementar htlm

--- Carta.php
+++ Carta.php
@@ -5,8 +5,39 @@
     <title>Carta</title>
 </head>
 <body>
-    <?php
-        include_once("Header.html");
-    ?>
+    <div>
+        <?php
+            include_once("Header.html");
+        ?>
+    </div>
+    <div id="seccion_carta">
+        <nav id="menu_comida">
+            <ul>
+                <li>Medias</li>
+                <li>Combinados</li>
+                <li>Carnes</li>
+                <li>Pescados</li>
+                <li>Bebidas</li>
+                <li>Postres</li>
+            </ul>
+        </nav>
+        <table id="tablas_carta">
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Strock</th>
+            </tr>
+            <tr>
+                <td>Ejemplo 1</td>
+                <td>Ejemplo 2</td>
+                <td>Ejemplo 3</td>
+            </tr>
+            <tr>
+                <td>Ejemplo 1</td>
+                <td>Ejemplo 2</td>
+                <td>Ejemplo 3</td>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
