d2c99c670f1b27265827fed60961b0d4b5e8e7a1
Juanmagc99 <juagarcri@alum.us.es>
2020-04-26 00:03:53 +0200

    necesita revision falla el js

--- Carta.php
+++ Carta.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Carta</title>
 </head>
 <body>
@@ -10,34 +10,202 @@
             include_once("Header.html");
         ?>
     </div>
-    <div id="seccion_carta">
-        <nav id="menu_comida">
-            <ul>
-                <li>Medias</li>
-                <li>Combinados</li>
-                <li>Carnes</li>
-                <li>Pescados</li>
-                <li>Bebidas</li>
-                <li>Postres</li>
-            </ul>
-        </nav>
-        <table id="tablas_carta">
+
+    <div class="tab_carta_inicio">
+        <button class="carta_button" onclick="openCarta(event, 'Medias')">
+            Medias
+        </button>
+        <button class="carta_button" onclick="openCarta(event, 'Combinados')">
+            Combinados
+        </button>
+        <button class="carta_button" onclick="openCarta(event, 'Carnes')">
+            Carnes
+        </button>
+        <button class="carta_button" onclick="openCarta(event, 'Pescados')">
+            Pescados
+        </button>
+        <button class="carta_button" onclick="openCarta(event, 'Bebidas')">
+            Bebidas
+        </button>
+        <button class="carta_button" onclick="openCarta(event, 'Postres')">
+            Postres
+        </button>
+    </div>
+
+    <div id="Medias" class="tab_carta_cont">
+        <h3>Medias</h3>
+        <table>
             <tr>
                 <th>Nombre</th>
                 <th>Precio</th>
-                <th>Strock</th>
+                <th>Stock</th>
             </tr>
             <tr>
-                <td>Ejemplo 1</td>
-                <td>Ejemplo 2</td>
-                <td>Ejemplo 3</td>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
             </tr>
             <tr>
-                <td>Ejemplo 1</td>
-                <td>Ejemplo 2</td>
-                <td>Ejemplo 3</td>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
             </tr>
         </table>
     </div>
+
+    <div id="Combinados" class="tab_carta_cont">
+        <h3>Cambinados</h3>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Stock</th>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+        </table>
+    </div>
+
+    <div id="Carnes" class="tab_carta_cont">
+        <h3>Carnes</h3>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Stock</th>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+        </table>
+    </div>
+
+    <div id="Pescados" class="tab_carta_cont">
+        <h3>Pescados</h3>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Stock</th>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+        </table>
+    </div>
+
+    <div id="Bebidas" class="tab_carta_cont">
+        <h3>Bebidas</h3>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Stock</th>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+        </table>
+    </div>
+
+    <div id="Postres" class="tab_carta_cont">
+        <h3>Postres</h3>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Stock</th>
+                <th>Stock</th>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+            <tr>
+                <td>Ejemplo</td>
+                <td>999</td>
+                <td>999</td>
+            </tr>
+        </table>
+    </div>
+
+    <script>
+        function openCarta(evt, tablaProducto) {
+            var i, tab_cart_cont, carta_button;
+
+            tab_cart_cont = document.getElementsByClassName("tab_cart_cont");
+            for (i = 0; i < tab_cart_cont.length; i++){
+                tab_cart_cont[i].style.display = "none";
+            }
+
+            carta_button = document.getElementsByClassName("carta_button");
+            for (i = 0; i < tab_cart_cont.length; i++){
+                carta_button[i].className = carta_button[i].className.replace(" active","")
+            }
+
+            document.getElementById(tablaProducto).style.display = "block";
+            evt.currentTarget.className += " active";
+        }
+    </script>
 </body>
 </html>
